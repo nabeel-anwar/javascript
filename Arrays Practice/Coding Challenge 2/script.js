4cc4090f894a9f5bf855199f2d9f31900bdb90a1
nabeel-anwar <nabeelanwar1965@gmail.com>
2022-07-20 18:49:26 +0500

    Coding Challenge 2

--- Arrays Practice/Coding Challenge 2/script.js
+++ Arrays Practice/Coding Challenge 2/script.js
@@ -1,79 +1,6 @@
-'use strict';
+"use strict";
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
-
-// Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
-
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
-
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
-
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
-
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
+//Coding Challenge 2
 
 /* Let's go back to Julia and Kate's study about dogs. This time, they want to convert 
 dog ages to human ages and calculate the average age of the dogs in their study.
@@ -94,18 +21,18 @@ Test data:
 GOOD LUCK � */
 
 const calcAverageHumanAge = function (ages) {
-
   console.log(`Dogs Ages: ${ages}`);
 
-  const humanAge = ages.map(age => age <= 2 ? (2 * age) : (16 + age * 4));
+  const humanAge = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
   console.log(`Human Ages: ${humanAge}`);
 
-  const adults = humanAge.filter(humanAge => humanAge >= 18);
+  const adults = humanAge.filter((humanAge) => humanAge >= 18);
   console.log(`Filter Ages: ${adults}`);
 
-  const average = adults.reduce((acc, humanAge) => acc + humanAge, 0) / adults.length;
+  const average =
+    adults.reduce((acc, humanAge) => acc + humanAge, 0) / adults.length;
   console.log(average);
-}
+};
 
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
