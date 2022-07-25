88fc3873fc1fe5ac77da4a9dd2cee69a70588d8f
nabeel-anwar <nabeelanwar1965@gmail.com>
2022-07-25 18:31:16 +0500

    DSA and Modern Operators

---
+++ DSA Practice/DSA Coding Challange 1/script.js
@@ -0,0 +1,97 @@
+'use strict';
+
+/*
+1. Create one player array for each team (variables 'players1' and 
+'players2')
+2. The first player in any player array is the goalkeeper and the others are field 
+players. For Bayern Munich (team 1) create one variable ('gk') with the 
+goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 
+field players
+3. Create an array 'allPlayers' containing all players of both teams (22 
+players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a 
+new array ('players1Final') containing all the original team1 players plus 
+'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 
+'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player 
+names (not an array) and prints each of them to the console, along with the 
+number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which 
+team is more likely to win, without using an if/else statement or the ternary 
+operator.
+Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. 
+Then, call the function again with players from game.scored
+GOOD LUCK �
+
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+    'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+}
+
+//Task 01
+const [players1, players2] = game.players;
+
+//Task 02
+const [team1Gk, ...team1FieldPlayers] = players1;
+const [team2Gk, ...team2FieldPlayers] = players2;
+
+//Task 03
+const allPlayers = [...players1, ...players2];
+
+//Task 04
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+//Task 05
+const { odds: { team1, x: draw, team2 } } = game;
+
+//Task 06
+
+const printGoals = function (...players) {
+  console.log(`${players.length} were totaly scored`);
+};
+
+printGoals(...game.scored);
+
+//Task 07
+team1 < team2 && console.log("Team 1 win");
+team1 > team2 && console.log("Team 2 win");
